d4cc0017400155ec39965facd17750a1b6bf5110
Sagar Patel <Sam@Sagars-MacBook-Pro.local>
2018-06-26 18:43:44 -0400

    added npm support

---
+++ Scripts/core/game.js
@@ -0,0 +1,24 @@
+(function(){
+
+    function Init()
+    {
+        console.log("Initialization Started.");
+    }
+
+    function Start()
+    {
+    
+    }
+
+    function Update()
+    {
+
+    }
+
+    function Main()
+    {
+
+    }
+
+    window.onload = Init;
+})();
